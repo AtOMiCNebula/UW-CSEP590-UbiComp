1392902e23ef2be663dd12a763b9dccf5c5eb9e6
Jeff Weiner <jdweiner@cs.washington.edu>
2015-04-15 20:25:02 -0700

    Create empty project for Assignment 2
    Adapted from HelloVision2.zip

---
+++ Assignment2/app/src/main/java/works/com/hellovision2/VisionActivity.java
@@ -0,0 +1,146 @@
+package works.com.hellovision2;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+
+public class VisionActivity extends ActionBarActivity implements CameraBridgeViewBase.CvCameraViewListener2{
+
+    String TAG = "APP";
+    CameraBridgeViewBase mOpenCvCameraView;
+    float lastTouchY=0;
+    int cannyThreshold=50;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "called onCreate");
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_vision);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.HelloOpenCvView);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+
+    }
+
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
+    }
+
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_vision, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+//
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+//
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        Mat currentFrame = inputFrame.rgba();
+//        Core.mean(currentFrame); -> returns x,y,z,a
+        Mat gryFrame = new Mat();
+        Imgproc.cvtColor(currentFrame, currentFrame, Imgproc.COLOR_RGBA2GRAY);
+
+        Imgproc.Canny(currentFrame,currentFrame,cannyThreshold/3,cannyThreshold );
+        return currentFrame;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+//        // MotionEvent reports input details from the touch screen
+//        // and other input controls. In this case, you are only
+//        // interested in events where the touch position changed.
+        float y = e.getY();
+        if (e.getAction() == MotionEvent.ACTION_MOVE) {
+            if (lastTouchY > y)
+                cannyThreshold += 5;
+            else
+                cannyThreshold -= 5;
+
+            lastTouchY = y;
+        }
+//
+        if (e.getAction() == MotionEvent.ACTION_UP)
+            lastTouchY = 0;
+        return true;
+    }
+
+
+}
